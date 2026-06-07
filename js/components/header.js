53f6c4dc9816dfd4e03335346198a376f5749512
Amir Rafati <97825338+amirafa@users.noreply.github.com>
2026-06-07 14:20:33 +0330

    Undo `index`

--- js/components/header.js
+++
@@ -1,7 +0,0 @@
-const template = await fetch('./js/components/header.html').then(res => res.text());
-
-export default {
-    name: 'ChatHeader',
-    emits: ['new-chat'],
-    template: template
-};
